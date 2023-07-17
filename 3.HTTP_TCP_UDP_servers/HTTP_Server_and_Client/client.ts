fb6117a9a1fcc09f603427107e8d9c7232f8e6ce
Yaroslav Romaniuk <romaniuk007@gmail.com>
2023-07-18 03:53:58 +0400

    HTTP done

--- 3.HTTP_TCP_UDP_servers/HTTP_Server_and_Client/client.ts
+++ 3.HTTP_TCP_UDP_servers/HTTP_Server_and_Client/client.ts
@@ -1,7 +1,10 @@
 import http from 'http';
 
+// The data to be sent in the request body
 const postData = 'Hello, Server!';
+const timestamp = new Date().toISOString();
 
+// Options for the HTTP request
 const options = {
   hostname: 'localhost',
   port: 3000,
@@ -9,30 +12,42 @@ const options = {
   method: 'POST',
   headers: {
     'Content-Type': 'text/plain',
-    'Content-Length': Buffer.byteLength(postData)
+    'Content-Length': Buffer.byteLength(postData) // Calculate the length of the request body
   }
 };
 
+// Send an HTTP request to the server
 const req = http.request(options, (res) => {
   let responseData = '';
+  
 
+  // Handle the data received from the server
   res.on('data', (chunk) => {
     responseData += chunk;
   });
 
+  // Handle the end of the response
   res.on('end', () => {
-    const endTime = performance.now();
-    const elapsedTime = endTime - startTime;
-    console.log('Response:', responseData);
-    console.log(`Elapsed Time: ${elapsedTime.toFixed(2)} milliseconds`);
+    const endTime = performance.now(); // Get the end time of the response
+    const elapsedTime = endTime - startTime; // Calculate the elapsed time
+    console.log(`[${timestamp}] Response:`, responseData);
+    if (postData === responseData){
+      console.log(`[${timestamp}] REQUEST AND RESPONSE MASSAGES IS EQUAL`);
+    } else {
+      console.log(`[${timestamp}] ERROR: REQUEST AND RESPONSE MASSAGES IS NOT EQUAL`);
+    }
+    console.log(`[${timestamp}] Elapsed Time: ${elapsedTime.toFixed(2)} milliseconds`);
   });
 });
 
+// Handle errors that occur during the request
 req.on('error', (error) => {
   console.error('Error:', error);
 });
 
-const startTime = performance.now();
+const startTime = performance.now(); // Get the start time of the request
 
+// Write the request body data and end the request
+console.log(`[${timestamp}] Request: ${postData}`)
 req.write(postData);
 req.end();
--- 3.HTTP_TCP_UDP_servers/HTTP_Server_and_Client/server.ts
+++ 3.HTTP_TCP_UDP_servers/HTTP_Server_and_Client/server.ts
@@ -3,23 +3,51 @@ import http from 'http';
 const hostname = 'localhost';
 const port = 3000;
 
+// Create an HTTP server
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!');
+  const startTime = performance.now(); // Start measuring the response time
+  const timestamp = new Date().toISOString();
+  let requestData = '';
+
+  // Listen for data chunks received from the client
+  req.on('data', (chunk) => {
+    requestData += chunk;
+    const clientIP = req.socket.remoteAddress; // Get the client's IP address
+    
+    console.log(`[${timestamp}] Client IP: ${clientIP}`);
+  });
+
+  // Listen for the end of the request
+  req.on('end', () => {
+    console.log(`[${timestamp}] Request Body: ${requestData}`);
+
+    // Set the response status code and headers
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    // Send back the received request data as the response
+    res.end(requestData);
+
+    console.log(`[${timestamp}] Response Sent`);
+
+    const endTime = performance.now(); // Get the end time of the response
+    const elapsedTime = endTime - startTime; // Calculate the elapsed time
+    console.log(`[${timestamp}] Elapsed Time: ${elapsedTime.toFixed(2)}`);
+  });
 });
 
+// Start the server and listen for incoming requests
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 // Function to gracefully shutdown the server
-function shutdownServer(): void {
-    server.close(() => {
-      console.log('Server is shutting down.');
-      process.exit(0);
-    });
-  }
-  
-  // Listen for SIGINT signal (Ctrl+C) to initiate server shutdown
-  process.on('SIGINT', shutdownServer)
+function shutdownServer() {
+  server.close(() => {
+    console.log('Server is shutting down.');
+    process.exit(0);
+  });
+}
+
+// Listen for SIGINT signal (Ctrl+C) to initiate server shutdown
+process.on('SIGINT', shutdownServer);

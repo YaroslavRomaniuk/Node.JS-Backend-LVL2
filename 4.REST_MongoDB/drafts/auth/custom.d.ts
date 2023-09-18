a52eed3aad0f929783a294e891e2db5bc4086bd9
Yaroslav Romaniuk <romaniuk007@gmail.com>
2023-09-18 23:16:57 +0400

    +

--- 4.REST_MongoDB/drafts/auth/custom.d.ts
+++
@@ -1,8 +0,0 @@
-import session from "express-session";
-
-declare module "express-session" {
-    export interface SessionData {
-        // your custom properties here
-        login: { [key: string]: any }; // example property
-    }
-}
